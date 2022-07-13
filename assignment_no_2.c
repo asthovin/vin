9023e9560cb58a901d5ee28c69708d81a276ac3f
vinay_prajapati_ <109099944+asthovin@users.noreply.github.com>
2022-07-13 19:06:25 +0530

    Add files via upload

---
+++ assignment_no_2.c
@@ -0,0 +1,190 @@
+// *** prob no 1 ***
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b;
+//     printf(" Enter a no. :- ");
+//     scanf(" %d ", &a);
+//     b = a / 10;
+//     printf(" %d", b);
+//     return 0;
+// }
+
+// **** prob no. 2 ****
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a, b;
+
+//     printf("Enter a no. :- ");
+//     scanf("%d", &a);
+//     b = a % 10;
+//     printf("The last digit of the given no. is :- %d", b);
+
+//     return 0;
+// }
+
+// **** prob no. 3 ****
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b, c;
+
+//     printf("Enter the first no.. :- ");
+//     scanf("%d", &a);
+
+//     printf("Enter the secont no. :- ");
+//     scanf("%d", &b);
+
+//     c = a;
+//     a = b;
+//     b = c;
+//     printf(" THe swaped no. are %d %d ", a, b);
+
+//     return 0;
+// }
+
+// **** prob no. 4 ****
+
+// #include <stdio.h>
+// int main()
+// {
+
+//     int a, b;
+
+//     printf("Enter the first no.. :- ");
+//     scanf("%d", &a);
+
+//     printf("Enter the second no. :- ");
+//     scanf("%d", &b);
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+
+//     printf(" THe swaped no. are %d %d ", a, b);
+
+//     return 0;
+// }
+
+// **** prob no. 5 ****
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b;
+//     printf(" Enter a three digit no. :- ");
+//     scanf(" %d", &a);
+//     b = a / 100 + a / 10 % 10 + a % 10;
+//     printf("%d", b);
+//     return 0;
+// }
+
+// **** prob no. 6 ****
+
+// #include <stdio.h>
+// int main()
+// {
+//     char a;
+//     printf("Enter the no. :- ");
+//     scanf("%c", &a);
+//     printf(" The ASCII code of the no. is %c = %d", a, a);
+//     return 0;
+// }
+
+// **** prob no. 8 ****
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b;
+
+//     printf(" Enter a no.  ");
+//     scanf("%d", &a);
+//     if (a % 2 == 0)
+//     {
+//         printf("THe no is even");
+//     }
+//     else
+//     {
+//         printf("THe no is odd");
+//     }
+
+//     return 0;
+// }
+
+// **** prob no. 9 ****
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b;
+//     b = sizeof(a);
+//     printf("%d", b);
+
+//         return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     float a, b;
+//     b = sizeof(a);
+//     printf("%f", b);
+
+//     return 0;
+// }
+
+//****************************************************** #include <stdio.h>
+// int main()
+// {
+//     char a, b;
+//     b = sizeof(a);
+//     printf("%c", b);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     double a, b;
+//     b = sizeof(a);
+//     printf("%lf", b);
+
+//     return 0;
+// }
+
+
+
+// **** prob no. 10 ****
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b;
+//     printf(" Enter the no. :- ");
+//     scanf(" %d", &a);
+//     b = a / 10;
+//     b = b * 10;
+//     printf(" %d", b);
+//     return 0;
+// }
+
+
+
+
+// *** prob no. 12 ***
+
+// #include <stdio.h>
+// int main()
+// {
+
+//     int a;
+//     float b;
+//     printf(" Enter the the value of INR :- ");
+//     scanf("%d", &a);
+//     b = a * 76.23;
+//     printf(" the USD of %d is %f", a, b);
+//     return 0;
+// }

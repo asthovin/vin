40e0ac3b1abae153dcff19ec46c31789723c2141
vinay_prajapati_ <109099944+asthovin@users.noreply.github.com>
2022-07-18 23:17:26 +0530

    Add files via upload

---
+++ Assignment_no_4.c
@@ -0,0 +1,143 @@
+// ***** problem no.1 ******
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1;
+//     while (i <= 5)
+//     {
+//         printf(" MySirG\n");
+//         i++;
+//     }
+
+//     return 0;
+// }
+// ***** problem no. 2******
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1;
+//     while (i <= 10)
+//     {
+//         printf("%d\n", i);
+//         i++;
+//     }
+
+//     return 0;
+// }
+
+// ***** problem no. 3******
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 10;
+//     while (i >= 1)
+//     {
+//         printf("%d\n", i);
+//         i--;
+//     }
+
+//     return 0;
+// }
+// ***** problem no. 4******
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1;
+//     while (i <= 20)
+//     {
+//         printf("%d\n", i);
+//         i += 2;
+//     }
+
+//     return 0;
+// }
+// ***** problem no. 5******
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 19;
+//     while (i >= 1)
+//     {
+//         printf("%d\n", i);
+//         i -= 2;
+//     }
+
+//     return 0;
+// }
+
+// ***** problem no. 6******
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 2;
+//     while (i <= 20)
+//     {
+//         printf("%d\n", i);
+//         i += 2;
+//     }
+
+//     return 0;
+// }
+
+// ***** problem no. 7*****
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 20;
+//     while (i >= 2)
+//     {
+//         printf("%d\n", i);
+//         i -= 2;
+//     }
+
+//     return 0;
+// }
+
+// ***** problem no. 8*****
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1;
+//     while (i <= 10)
+//     {
+//         printf("%d\n", i * i);
+//         i++;
+//     }
+
+//     return 0;
+// }
+
+// ***** problem no. 9*****
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1;
+//     while (i <= 10)
+//     {
+//         printf("%d\n", i * i * i);
+//         i++;
+//     }
+
+//     return 0;
+// }
+
+// ***** problem no. 10 ****
+
+#include <stdio.h>
+int main()
+{
+    int i = 1, x;
+    while (i <= 10)
+    {
+        x = 5;
+        printf("%d\n", x * i);
+        i++;
+    }
+
+    return 0;
+}

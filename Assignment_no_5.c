2d8de13d51b7e2e63515d63fd4b79fcb19d302d0
vinay_prajapati_ <109099944+asthovin@users.noreply.github.com>
2022-07-18 23:17:59 +0530

    Add files via upload

---
+++ Assignment_no_5.c
@@ -0,0 +1,159 @@
+
+// ***** problem no. 1 ****
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1, n;
+//     printf(" Enter the value that you want to print MySirG :- ");
+//     scanf(" %d", &n);
+//     while (i <= n)
+//     {
+
+//         printf("MySirG\n");
+//         i++;
+//     }
+
+//     return 0;
+// }
+
+// ***** problem no. 2 ****
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1, n;
+
+//     printf(" Enter the value of n :- ");
+//     scanf(" %d", &n);
+
+//     while (i <= n)
+//     {
+//         printf("%d\n", i);
+//         i++;
+//     }
+
+//     return 0;
+// }
+
+// ***** problem no. 3 ****
+
+// #include <stdio.h>
+// int main()
+// {
+//     int n;
+//     printf(" Enter the value of n :- ");
+//     scanf(" %d", &n);
+//     int i = n;
+
+//     while (i >= 0)
+//     {
+//         printf("%d\n", i);
+//         --i;
+//     }
+
+//     return 0;
+// }
+//  ***** problem no. 4****
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1, n;
+//     printf(" Enter the value of n :- ");
+//     scanf("%d", &n);
+//     while (i <= n)
+//     {
+//         printf("%d\n", 2 * i - 1);
+//         i++;
+//     }
+
+//     return 0;
+// }
+
+// / *****problem no.5 * ***
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i;
+//     printf(" Enter a number :- ");
+//     scanf("%d", &i);
+//     while (i)
+//     {
+//         printf("%d\n", 2 * i - 1);
+//         i--;
+//     }
+
+//     return 0;
+// }
+
+//  ***** problem no. 6****
+
+#include <stdio.h>
+int main()
+{
+    int i = 1, n;
+    printf(" Enter the value of n :- ");
+    scanf("%d", &n);
+
+    while (i <= n)
+    {
+
+        printf("%d\n", 2 * i);
+        i++;
+    }
+
+    return 0;
+}
+
+// *********** prob no. 8 *****
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1, n;
+
+//     printf(" Enter the value of n :- ");
+//     scanf(" %d", &n);
+
+//     while (i <= n)
+//     {
+//         printf("%d\n", i * i);
+//         i++;
+//     }
+
+//     return 0;
+// }
+
+// *********** prob no. 9 *****
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1, n;
+
+//     printf(" Enter the value of n :- ");
+//     scanf(" %d", &n);
+
+//     while (i <= n)
+//     {
+//         printf("%d\n", i * i * i);
+//         i++;
+//     }
+
+//     return 0;
+// }
+
+// *********** prob no. 10 *****
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1, n;
+//     printf(" Enter a no. for table :- ");
+//     scanf("%d", &n);
+//     while (i <= 10)
+//     {
+//         printf("%d\n", i * n);
+//         i++;
+//     }
+
+//     return 0;
+// }

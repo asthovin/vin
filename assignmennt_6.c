7b38ce2340a539a1cfc0510dc0d195bd0b19257f
vinay_prajapati_ <109099944+asthovin@users.noreply.github.com>
2022-07-26 22:50:22 +0530

    Add files via upload

---
+++ assignmennt_6.c
@@ -0,0 +1,96 @@
+
+// **** prob no. 1 ****
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i, sum = 0, a, b, c;
+//     printf("Enter a no. :- ");
+//     scanf("%d", &a);
+//     for (i = 1; i <= a; i++)
+//     {
+
+//         sum = sum + i;
+//     }
+//     printf(" THe sum of first %d natural number is %d", a, sum);
+//     return 0;
+// }
+
+// ***** prob 2******
+// #include <stdio.h>
+// int main()
+// {
+//     int n, s = 0, i = 1;
+//     printf("Enter a no. :- ");
+//     scanf("%d", &n);
+//     while (i <= n)
+//     {
+//         s = s + 2 * i;
+//         i++;
+//     }
+//     printf(" sum is %d", s);
+//     return 0;
+// }
+
+// // ***** prob 3******
+// #include <stdio.h>
+// int main()
+// {
+//     int n, s = 0, i = 1;
+//     printf("Enter a no. :- ");
+//     scanf("%d", &n);
+//     while (i <= n)
+//     {
+//         s = s + 2 * i - 1;
+//         i++;
+//     }
+//     printf(" sum is %d", s);
+//     return 0;
+// }
+
+// ***** prob 4******
+// #include <stdio.h>
+// int main()
+// {
+//     int n, s = 0, i = 1;
+//     printf("Enter a no. :- ");
+//     scanf("%d", &n);
+//     while (i <= n)
+//     {
+//         s = s + i * i;
+//         i++;
+//     }
+//     printf(" sum is %d", s);
+//     return 0;
+// }
+
+// ***** prob 5******
+// #include <stdio.h>
+// int main()
+// {
+//     int n, s = 0, i = 1;
+//     printf("Enter a no. :- ");
+//     scanf("%d", &n);
+//     while (i <= n)
+//     {
+//         s = s + i * i * i;
+//         i++;
+//     }
+//     printf(" sum is %d", s);
+//     return 0;
+// }
+
+// ********  problem no. 6 ****
+// #include <stdio.h>
+// int main()
+// {
+//     int i, a = 1, b;
+//     printf("Enter a number:- ");
+//     scanf("%d", &b);
+//     for (i = 1; i <= b; i++)
+//     {
+//         a = a * i;
+//     }
+//     printf("Factorial of %d is: %d", b, a);
+//     return 0;
+// }
